b757a4772c35d7e1aa0d0833515240dc6ddcc1ea
Cocoagoat <denis021299@gmail.com>
2024-10-17 12:23:12 +0300

    Moved redirect logic from client to server-side

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ export function middleware(request: NextRequest) {
   // Redirects users who change the URL to the username in the cookie
 
   const { pathname } = request.nextUrl;
+  console.log("pathname", pathname);
   const origin = request.nextUrl.origin;
 
   if (pathname.includes("/affinity")) {
@@ -21,7 +22,7 @@ export function middleware(request: NextRequest) {
   let split_req = request.nextUrl.href.split("/");
   const username = split_req[split_req.length - 2];
 
-  if (usernameCookie != username) {
+  if (usernameCookie != username && !pathname.includes("/home")) {
     if (usernameCookie) {
       split_req[split_req.length - 2] = usernameCookie;
       let new_url = split_req.join("/");
@@ -30,8 +31,34 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL(`${origin}/home`, request.url));
     }
   }
+
+  // Old redirection filtering, template might be useful in the future
+
+  // const matchPath = match("/:username", { decode: decodeURIComponent });
+  // console.log("matchPath", matchPath);
+  // const matchResult = matchPath(pathname);
+  // console.log("matchResult", matchResult);
+
+  // if (matchResult) {
+  //   const { username } = matchResult.params;
+
+  //   // If there's no additional segment (e.g. /username without /affinity, /recs, etc.), redirect to /home
+  //   if (
+  //     !pathname.includes("/home") &&
+  //     !pathname.includes("/affinity") &&
+  //     !pathname.includes("/recs") &&
+  //     !pathname.includes("/seasonal")
+  //   ) {
+  //     return NextResponse.redirect(new URL("/home", request.url));
+  //   }
+  // }
 }
 
 export const config = {
-  matcher: ["/:username/affinity", "/:username/recs", "/:username/seasonal"],
+  matcher: [
+    "/:username/affinity",
+    "/:username/recs",
+    "/:username/seasonal",
+    // "/:username/",
+  ],
 };
