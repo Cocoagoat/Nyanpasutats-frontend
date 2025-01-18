8b67631e11cd570c2a2ae0a65a4c757b012d931c
Cocoagoat <denis021299@gmail.com>
2025-01-19 01:11:59 +0200

    Added loading indicator for toolbar operations

--- app/[username]/seasonal/expanded/SeasonExpandedButton.tsx
+++ app/[username]/seasonal/expanded/SeasonExpandedButton.tsx
@@ -1,22 +1,22 @@
 import HoverPopup from "@/components/general/HoverPopup";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SingleSeasonContext } from "../reducer/SeasonalContext";
+import Loading from "@/components/general/Loading";
 
 export default function SeasonExpandedButton({
   onClick,
   Icon,
   hoverText,
-  hovered,
-  setHovered,
   open,
+  loading,
 }: {
   onClick: () => void;
   Icon: React.ReactElement;
   hoverText: string;
-  hovered: boolean;
-  setHovered: React.Dispatch<React.SetStateAction<boolean>>;
   open?: boolean;
+  loading?: boolean;
 }) {
+  const [hovered, setHovered] = useState(false);
   const { altBackgroundColor } = useContext(SingleSeasonContext)!;
   const iconWithHandlers = React.cloneElement(Icon, {
     onMouseEnter: () => setHovered(true),
@@ -29,18 +29,24 @@ export default function SeasonExpandedButton({
   });
 
   return (
-    <button
-      className={`${hoverText.startsWith("E") ? "bg-zinc-500" : "bg-zinc-700"} relative flex
-       items-center justify-center  bg-opacity-0 text-2xl `}
-      onClick={onClick}
-    >
-      {iconWithHandlers}
-      <HoverPopup
-        hovered={hovered}
-        setHovered={setHovered}
-        text={hoverText}
-        backgroundColor={altBackgroundColor}
-      />
-    </button>
+    <>
+      {!loading ? (
+        <button
+          className={`${hoverText.startsWith("E") ? "bg-zinc-500" : "bg-zinc-700"}
+       relative flex items-center justify-center  bg-opacity-0 text-2xl `}
+          onClick={onClick}
+        >
+          {iconWithHandlers}
+          <HoverPopup
+            hovered={hovered}
+            setHovered={setHovered}
+            text={hoverText}
+            backgroundColor={altBackgroundColor}
+          />
+        </button>
+      ) : (
+        <Loading width={30} backgroundColor={altBackgroundColor} />
+      )}
+    </>
   );
 }
