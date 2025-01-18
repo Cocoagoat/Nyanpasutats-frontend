13d928b71085d864818f25fb3c8190265a5a890f
Cocoagoat <denis021299@gmail.com>
2025-01-19 01:11:59 +0200

    Added loading indicator for toolbar operations

--- app/[username]/seasonal/expanded/tierlist/TierListToolbarButton.tsx
+++ app/[username]/seasonal/expanded/tierlist/TierListToolbarButton.tsx
@@ -1,35 +1,49 @@
 import HoverPopup from "@/components/general/HoverPopup";
 import React, { useContext } from "react";
 import { SingleSeasonContext } from "../../reducer/SeasonalContext";
+import Loading from "@/components/general/Loading";
 
 export default function TierListToolbarButton({
   children,
   onClick,
   descText,
+  loading,
 }: {
   children: React.ReactNode;
   onClick: () => void;
   descText?: string;
+  loading?: boolean;
 }) {
   const [hovered, setHovered] = React.useState(false);
   const { altBackgroundColor } = useContext(SingleSeasonContext)!;
   return (
-    <button
-      className="relative bg-zinc-800 p-1 text-3xl  hover:bg-zinc-700"
-      onClick={onClick}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      style={{ color: altBackgroundColor }}
-    >
-      {children}
-      {hovered && descText && (
-        <HoverPopup
-          hovered={hovered}
-          setHovered={setHovered}
-          text={descText}
+    <>
+      {!loading ? (
+        <button
+          className="relative bg-zinc-800 p-1 text-3xl  hover:bg-zinc-700"
+          onClick={onClick}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          style={{ color: altBackgroundColor }}
+        >
+          {children}
+          {hovered && descText && (
+            <HoverPopup
+              hovered={hovered}
+              setHovered={setHovered}
+              text={descText}
+              backgroundColor={altBackgroundColor}
+            />
+          )}
+        </button>
+      ) : (
+        <Loading
+          width={30}
+          height={30}
           backgroundColor={altBackgroundColor}
+          spinnerType="Regular"
         />
       )}
-    </button>
+    </>
   );
 }
